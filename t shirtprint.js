f1ac3047a96bffe392f4019557bbd01a50712ab1
KelvinSelorm <131010014+Kelvinswebdev@users.noreply.github.com>
2026-05-27 00:24:03 +0000

    update

---
+++ t shirtprint.js
@@ -0,0 +1,18 @@
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.createElement('div');
+lightbox.classList.add('lightbox');
+document.body.appendChild(lightbox);
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = image.src;
+    lightbox.innerHTML = '';
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.remove('active');
+});
